08683b6a34e1e844524dd90ad3c70f2db899a156
felixschmitz <felix.schmitz@gmail.com>
2017-02-24 17:36:12 +1100

    Added rest endpoint.

---
+++ src/main/java/org/openshift/JaxrsConfig.java
@@ -0,0 +1,10 @@
+package org.openshift;
+
+/**
+ * @author felixschmitz
+ * @since 1.0
+ */
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+@ApplicationPath("/api")
+public class JaxrsConfig extends Application{ }
